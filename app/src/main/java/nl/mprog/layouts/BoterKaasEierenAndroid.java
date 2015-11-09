c389e07805d004913b5720586e347a1f90a1c998
Joost Bremmer <toost.b@gmail.com>
2015-11-09 03:59:16 +0100

    Full Implementation of Tic-Tac-Toe
    + BoterKaasEieren
      + Added hasWin() to check for winning conditions
      + Added resetBoard() to clear the board once someone's won
      + Added wins[] and addWin() to keep track of wins per round
      + Added turn and getTurn() to keep track of turns, in case of a tie
      + Various helper functions for hasWon and others
      + Javadoc for all.
    
    + BoterKaasEierenAndroid
      + Moved translating Viewtags to co-ordinates to readTile()
      + Added printScore() to show message in case of win
      + tweaked drawBoard() for re-use after initiating the board and
        respect boardSize
      + Javadoc for all.
    
    + BoterKaasMain
      + Tweaked playTurn() to check for win after turn, and respond
      appropriately
      + Remove references to '3' and
      BoterKaasEierenAndroid.DEFAULT_BOARDSIZE
      + Javadoc for some.
    
    + activity_main.xml
      + cosmetic changes
    
    Signed-off-by: Joost Bremmer <toost.b@gmail.com>

--- app/src/main/java/nl/mprog/layouts/BoterKaasEierenAndroid.java
+++ app/src/main/java/nl/mprog/layouts/BoterKaasEierenAndroid.java
@@ -2,34 +2,54 @@ package nl.mprog.layouts;
 
 
 import android.content.Context;
-import android.content.res.Resources;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.GridLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.Snackbar;
 
 
+/**
+ * sub-class of {@link BoterKaasEieren}
+ * Handles the visualisation and user-interaction for Android.
+ *
+ * @author Joost Bremmer
+ * @version 1.0
+ */
 public class BoterKaasEierenAndroid extends BoterKaasEieren {
 
-    /** Was Trying to be FANCY, crashes app somehow. Can't find the resource
+    /* Was Trying to be FANCY, crashes app somehow. Can't find the resource
     //@Override
     public char setTile(int row, int column) {
 
         String x = Resources.getSystem().getString(R.string.boter);
         String o = Resources.getSystem().getString(R.string.kaas);
 
-        char turn = isTurnX() ? x.charAt(0) : o.charAt(0);
+        char turn = getPlayer();
         board[row][column] = turn;
         return turn;
-    }
+    } */
 
-    **/
 
-    public void drawBoard(Context context, android.support.v7.widget.GridLayout boardGrid,
-                          int boardSize)
+    /**
+     * Draws a representation of the {@link #board board} to the screen using a GridLayout and a Button
+     * view. Also sets a tag to each (Button) tile containing it's position on the board.
+     *
+     * @param context   The current {@link Context context} of your
+     *                              {@link android.app.Activity activity}.
+     * @param boardGrid a {@link GridLayout grid} in which to draw the board.
+     * @see Context
+     * @see android.support.v7.widget.GridLayout
+     * @see Button
+     */
+    public void drawBoard(Context context, android.support.v7.widget.GridLayout boardGrid)
     {
+
+        boardGrid.removeAllViews();
+        boardGrid.setColumnCount(boardSize);
+        boardGrid.setRowCount(boardSize);
 
         for (int i=0;i< boardSize;i++) {
 
@@ -48,12 +68,26 @@ public class BoterKaasEierenAndroid extends BoterKaasEieren {
 
         for(int i=0; i<boardGrid.getChildCount(); i++) {
             View boardTile = boardGrid.getChildAt(i);
-            Log.v("child at " + i + ":", String.valueOf(boardTile.getTag()));
             android.support.v7.widget.GridLayout.MarginLayoutParams params =
                     (android.support.v7.widget.GridLayout.MarginLayoutParams)
                             boardTile.getLayoutParams();
             params.setMargins(4,4,4,4);
         }
+    }
+
+
+    /**
+     * Read a views position from it's tag.
+     *
+     * @param tile View; Must have a tag containing the tile's position of the board
+     *                   in the format of 'row'-'column'. For example, '0-2' is the top center.
+     * @return     int[]; position as an array, contains the row as the first number,
+     *                                                   column as the second number.
+     */
+    public int[] readTile(View tile) {
+
+        return (new int[]{Character.getNumericValue(String.valueOf(tile.getTag()).charAt(0)),
+                          Character.getNumericValue(String.valueOf(tile.getTag()).charAt(2))});
     }
 
 
@@ -64,15 +98,25 @@ public class BoterKaasEierenAndroid extends BoterKaasEieren {
      *             For example: 2-0 would be the bottom-left tile on a 3 by 3 board.
      */
     public char playTurn(Button tile) {
-        int row = Character.getNumericValue(String.valueOf(tile.getTag()).charAt(0));
-        int column = Character.getNumericValue(String.valueOf(tile.getTag()).charAt(2));
+        int position[] =readTile(tile);
 
-        char turn = setTile(row, column);
+        char turn = setTile(position[0], position[1]);
 
-        Log.v("Set tile " + row + '-' + column + "to: ", String.valueOf(turn));
-        Log.v("double check:", String.valueOf(getTile(row,column)));
+        Log.v("Set tile " + position[0] + '-' + position[1] + " to", String.valueOf(turn));
+        //Log.v("double check", String.valueOf(getTile(position[0],position[1])));
 
         return turn;
 
+    }
+
+    /**
+     * Shows a message on who has won the current round, and what the scores are.
+     * @param view View from which to get the parent from, to which the message is drawn.
+     * @see android.support.design.widget.Snackbar
+     */
+    public void printScore(View view) {
+        Snackbar.make(view, String.valueOf(getPlayer()) + " Wins!\n" +
+                "X has " + wins[0] + "wins" + "\t|\t" + "O has " + wins[1] + " wins",
+                Snackbar.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/nl/mprog/layouts/BoterKaasMain.java
+++ app/src/main/java/nl/mprog/layouts/BoterKaasMain.java
@@ -1,32 +1,41 @@
 package nl.mprog.layouts;
 
-import android.content.DialogInterface;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayout;
-import android.text.Layout;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.ListAdapter;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
-import org.w3c.dom.Text;
+
+/**
+ * Main Activty for a Tic-Tac-Toe Game
+ * @see BoterKaasEierenAndroid
+ * @see BoterKaasEieren
+ * @see android.app.Activity
+ *
+ * @author Joost Bremmer
+ * @version 1.0
+ *
+ */
 
 public class BoterKaasMain extends AppCompatActivity {
 
     android.support.v7.widget.GridLayout boterkaas;
     BoterKaasEierenAndroid game = new BoterKaasEierenAndroid();
 
+    /**
+     * draws MainActivity on creation, and also initiates the board.
+     * @param savedInstanceState Everything saved from previous instance, if any.
+     * @see AppCompatActivity
+     * @see Bundle
+     * @see BoterKaasEierenAndroid#drawBoard(Context, GridLayout)
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -45,7 +54,7 @@ public class BoterKaasMain extends AppCompatActivity {
         Log.v("test", String.valueOf(boterkaas));
 
 
-        game.drawBoard(this, boterkaas, BoterKaasEieren.DEFAULT_BOARDSIZE);
+        game.drawBoard(this, boterkaas);
 
 
     }
@@ -72,17 +81,47 @@ public class BoterKaasMain extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * OnClick Event for the tiles of the board
+     *
+     * <p>
+     * Handles setting clicked tile to the correct value ('X' or 'O', depending on whose turn it is)
+     * Then it checks if anyone has won, if so it shows an appropriate message and resets the board
+     * or goes to the next turn.
+     * </p>
+     * @param view clicked view, added automatically.
+     *
+     * @see BoterKaasEierenAndroid#playTurn(Button)
+     * @see BoterKaasEierenAndroid#readTile(View)
+     * @see BoterKaasEierenAndroid#printScore(View)
+     * @see BoterKaasEierenAndroid#resetBoard()
+     * @see BoterKaasEierenAndroid#drawBoard(Context, GridLayout)
+     * @see BoterKaasEieren#switchTurn()
+     */
     public void playTurn(View view) {
-        Log.v("buttan", String.valueOf(view.getTag()));
+        Log.v("turn", String.valueOf(game.getTurn()));
 
         boterkaas = (android.support.v7.widget.GridLayout) findViewById(R.id.boterkaas);
         Button tile = (Button) boterkaas.findViewWithTag(view.getTag());
 
         char result = game.playTurn(tile);
         tile.setText(String.valueOf(result));
-        Log.v("triple check:", String.valueOf(tile.getText()));
+        tile.setEnabled(false);
 
-        game.switchTurn();
+        int position[] = game.readTile(view);
+
+        if(game.hasWon(position[0],position[1])) {
+            game.printScore(view);
+            game.resetBoard();
+            game.drawBoard(view.getContext(),boterkaas);
+
+        }
+
+        else if (game.getTurn() == (game.boardSize*game.boardSize)) {
+            Snackbar.make(view, "A tie! Everybody loses!", Snackbar.LENGTH_SHORT).show();
+            game.resetBoard();
+            game.drawBoard(view.getContext(), boterkaas);
+        } else {game.switchTurn();}
 
     }
 }
