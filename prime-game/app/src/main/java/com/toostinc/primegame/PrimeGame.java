0d50e35b5a007dbb7983b6b1a69422d00ca47261
Joost Bremmer <toost.b@gmail.com>
2016-05-06 00:37:05 +0200

    Finally fix prime checking algorithm.

--- prime-game/app/src/main/java/com/toostinc/primegame/PrimeGame.java
+++ prime-game/app/src/main/java/com/toostinc/primegame/PrimeGame.java
@@ -56,7 +56,7 @@ public class PrimeGame {
       if ( n % 2 == 0) {return false;}
 
       else {
-        for (int i=3; i*i <= n; i++) {
+        for (int i=3; i*i <= n; i+=2) {
           if (n % i == 0)
             return false;
         }
